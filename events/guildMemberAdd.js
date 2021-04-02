977546070db620299fa5646351f7f32dd7ed1c34
KIBRA <62299912+EMR-KBR@users.noreply.github.com>
2021-04-02 20:07:06 +0300

    emr-kbr-fixed

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('https://media.giphy.com/media/eSQFIjNhmeX28/giphy.gif Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle https://gph.is/2VhWm6k :wink:');
+
+};
---
+++ events/ready.js
@@ -0,0 +1,25 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../config.js');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+ /*var oyun = [
+        "Ananın amıyla",
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "https://ibots.glitch.com" );
+        }, 2 * 2500); */
+    
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setActivity(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+};
