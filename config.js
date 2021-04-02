0f92d6a65d3fe30c27ae9045cba1c8f5ecc18b50
KIBRA <62299912+EMR-KBR@users.noreply.github.com>
2021-04-02 20:07:54 +0300

    emr-kbr-fixed

---
+++ config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  // Bot Bilgileri
+
+    "token": "ODE5MjQzMDY0MTIzODUwODAy.YEjxjw.v20VwJ9j8cnHO5JqwDvjduDoF50", // Discord Botunuzun Tokenini Girin.
+    "author": "765894055820263435", // Botun Sahibi ID
+    "prefix": ".", // Prefix Belirleyin.
+    "oynuyor": "NoNameRoleplay", // Bot Oynuyor Kısmı
+    "bot_durum": "PLAYING", // WATCHING, PLAYING
+    
+
+    // Sunucu Bilgileri
+
+     "fivem_server_name": "NoNameRoleplay", // Sunucunuzun Adını Girin
+     "fivem_server_logo": "https://cdn.discordapp.com/attachments/818712907424071712/818931280116711475/1024.png", // Sunucunuzun Logosunun Linkini Yerleştirin.
+     "fivem_server_url": "www.google.com", // Sunucunuzun URL sini giriniz.
+     "fivem_server_ip": "127.0.0.1", // Veya Launcher Kanalını etiketleyebilirsiniz. <#KANAL_ID> şeklinde.
+     "fivem_teamspeak_ip": "randomkey.com", // TS3 IP yazın
+     "fivem_server_staff_id": "<@&827112443947647006>", // Tüm Staff rollerini <@&ROL_DISCORD_ID> ROL_DISCORD_ID yerine staff rolünün idsini yazarak etikleyebilirsiniz.
+    
+
+    // Database Connect
+    "mysql_host": "localhost", // Host
+    "mysql_user": "root", // User
+    "mysql_pass": "", // DB Password Localhost üzerinde iseniz default olarak şifre yoktur.
+    "mysql_dbname": "hurt", // Database adı
+
+
+
+    "whitelist_command": "ekle", // Komut adı
+    "whitelist_error_message": "Whitelist Eklenemedi. Veritabanı bağlantınızı kontrol ediniz.", // Komut Mesajı
+    "whitelist_footer": "Whitelist Servisi", // Footer
+
+    // Sunucu Aktif Komutları
+    "server_aktif_command": "aktif", // Aktif Komutu 
+    "server_aktif_gif": "https://cdn.discordapp.com/attachments/818712907424071712/820757695371542548/giphy_1.gif", // Aktif Mesaj GIF i
+    "server_aktif_message": "Sunucu Aktiftir İyi Roller Dileriz", // Aktif Mesajı
+    "server_aktif_color": "0x000000", // or RANDOM
+
+
+    // Bakım Komutları
+
+    "server_bakım_command": "bakım",
+    "server_bakım_gif": "https://cdn.discordapp.com/attachments/818712907424071712/820760310532407317/giphy_2.gif", // Bakım Mesaj GIF
+    "server_bakım_message": "Sunucu Bakıma Alınmıştır. Anlayışınız için teşekkür ederiz.", // Bakım Mesajı
+    "server_bakım_color": "0x000000", // veya RANDOM yazın.
+
+
+    // Destek Ve Kayıt Çağırma
+    "destek_command": "destek", // Komut Adı
+    "destek_message": "Destek Talebiniz İletildi. Yetkililerimiz en kısa sürede size geri dönüş sağlayacaktır.", // Destek Mesajı
+    "destek_title": "NNP Destek Servisi", // Başlık
+    "destek_color": "0x000000", // veya RANDOM yazın.
+
+    // Kayıt
+    "kayit_cagirma_command": "kayıt", // Komut Adı
+    "kayit_message": "Kayıt Bildiriniz Yetkililere iletildi.", // Mesaj
+    "kayit_title": "NNRP Kayıt Bildiri", // Başlık
+    "kayit_color": "0x000000", // veya RANDOM yazın.
+
+    
+   "fivem_error_command": "hata",
+   "fivem_error_color": "0x000000", // Hata Bildiri Mesaj Rengi
+   "fivem_error_header": "NNP Hata Bildiri", // Hata Bildiri Mesaj Başlık
+   "fivem_error_description": "Sunucumuzda bulduğunuz hata iletildi. Bildirdiğiniz için teşekkür ederiz.", // Hata Bildiri Mesajı
+   "fivem_error_dsc2": "Yetkililerimiz size özelden geri dönüş sağlayacaktır.", // Hata bildiri mesajı 2
+   "fivem_error_channel": "827494024557625374" // Hata Bildiri Kanal ID sini yazın.
+};
